0c9c39f45a00ff5a5e082edeeefc0e1610f1349c
oscargarridoDUOC <osca.garrido@duocuc.cl>
2025-05-15 11:15:15 -0400

    Primer commit

---
+++ GesImp/src/main/java/GLISERV/GesImp/GesImpApplication.java
@@ -0,0 +1,13 @@
+package GLISERV.GesImp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesImpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesImpApplication.class, args);
+	}
+
+}
---
+++ GesImp/src/test/java/GLISERV/GesImp/GesImpApplicationTests.java
@@ -0,0 +1,13 @@
+package GLISERV.GesImp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesImpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
